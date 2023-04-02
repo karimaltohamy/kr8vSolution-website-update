079a571530a622b50687685ceae4462e616f389a
Karim Altohamy <karimbadr2003@gmail.com>
2023-04-03 00:35:00 +0200

    comit

--- js/main.js
+++ js/main.js
@@ -9,3 +9,29 @@ iconMenu.addEventListener("click", () => {
 closeMenu.addEventListener("click", () => {
   modelMenu.classList.remove("open");
 });
+
+const header = document.querySelector(".header");
+
+window.onscroll = function () {
+  // scroll navbar fixed
+  if (window.scrollY > 20) {
+    header.classList.add("header-scroll");
+  } else {
+    header.classList.remove("header-scroll");
+  }
+};
+
+// portfolio btn end
+
+// const btnEnd = document.querySelector(".btn-end");
+const nextSection = document.querySelector(".our_latest_work");
+
+console.log(nextSection.scrollTop);
+
+const horizontalScrollContainer = document.querySelector(".scroll");
+const content = document.getElementById("content");
+
+// window.addEventListener("scroll", function () {
+//   const scrollPosition = window.pageYOffset;
+//   horizontalScrollContainer.scrollLeft = -scrollPosition;
+// });
---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
